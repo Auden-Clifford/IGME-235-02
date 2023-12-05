3dea19a2111932a69bb65f25c978bd30d43f0bf5
Auden Clifford (RIT Student) <awc6002@rit.edu>
2023-12-05 12:15:28 -0500

    added title graphics

--- Projects/project3/js/main.js
+++ Projects/project3/js/main.js
@@ -20,6 +20,15 @@ const GameState =
     GameOver: 3
 }
 
+// pre-load the images (this code works with PIXI v6)
+app.loader.
+    add([
+        "images/title.svg"
+    ]);
+app.loader.onProgress.add(e => { console.log(`progress=${e.progress}`) });
+app.loader.onComplete.add(Setup);
+app.loader.load();
+
 // aliases
 let stage;
 
@@ -57,8 +66,57 @@ function Setup()
     // Create the `shop` scene and make it invisible
     shopScene = new PIXI.Container();
     shopScene.visible = false;
-    stage.appendChild(gameOverScene);
+    stage.addChild(gameOverScene);
 
     // Create labels for all 3 scenes
     createLabelsAndButtons();
+
+    // Start update loop
+    app.ticker.add(gameLoop);
+
+    // Start listening for click events on the canvas
+    app.view.onclick = fireBullet;
+}
+
+function createLabelsAndButtons()
+{
+    let textButtonStyle = new PIXI.TextStyle({
+        fill: 0xddd573,
+        fontSize: 48,
+        fontFamily: "Arial"
+    });
+
+    // start scene
+    // top title
+    let title = new PIXI.Sprite.from(app.loader.resources["images/title.svg"].texture);
+    title.x = 30;
+    title.y = 60;
+    startScene.addChild(title);
+
+    // help button
+    let helpButton = new PIXI.Text("HELP");
+    helpButton.style = textButtonStyle;
+    helpButton.x = 30;
+    helpButton.y = 240;
+    helpButton.interactive = true;
+    helpButton.buttonMode = true;
+    helpButton.on("pointerup", startGame); // function reference to startGame
+    helpButton.on('pointerover', e => e.target.alpha = 0.7);
+    helpButton.on('pointerout', e => e.currentTarget.alpha = 1.0);
+    startScene.addChild(helpButton);
+}
+
+function startGame(){
+    startScene.visible = false;
+    gameOverScene.visible = false;
+    gameScene.visible = true;
+
+    levelNum = 1;
+    score = 0;
+    life = 100;
+    increaseScoreBy(0);
+    decreaseLifeBy(0);
+    ship.x = 300;
+    ship.y = 550;
+    loadLevel();
 }
